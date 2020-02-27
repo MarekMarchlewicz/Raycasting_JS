d09b20cdf5880f68b763524ff712ba907b786d6c
Marek <marek.marchlewicz@gmail.com>
2020-02-27 19:14:26 +0800

    basic setup working

--- raycasting/raycast.js
+++ raycasting/raycast.js
@@ -35,19 +35,75 @@ class Map {
     }
 }
 
-var grid = new Map();
+class Player {
+    constructor() {
+        this.x = WINDOW_WIDTH / 2;
+        this.y = WINDOW_HEIGHT / 2;
+        
+        this.radius = 3;
+        this.turnDirection = 0; // -1 = left | 1 = right
+        this.walkDirection = 0; // -1 = back | 1 = forward
+        this.rotationAngle = Math.PI / 2; // 90 deg
+        this.moveSpeed = 1;
+        this.rotationSpeed = 2 * (Math.PI / 180);
+    }
 
+    render() {
+        noStroke();
+        fill("blue");
+        circle(this.x, this.y, this.radius);
+        stroke("blue");
+        line(this.x, this.y, this.x + Math.cos(this.rotationAngle) * 10, this.y + Math.sin(this.rotationAngle) * 10);
+    }
+
+    update()
+    {
+        this.rotationAngle += this.turnDirection * this.rotationSpeed;
+        this.x += Math.cos(this.rotationAngle) * this.moveSpeed * this.walkDirection;
+        this.y += Math.sin(this.rotationAngle) * this.moveSpeed * this.walkDirection;
+    }
+}
+
+var grid = new Map();
+var player = new Player();
+
+function keyUpEvent(e) {
+    console.log(e.key);
+    if(e.key =="ArrowUp"){
+        player.walkDirection = 0;
+    } else if(e.key == "ArrowDown") {
+        player.walkDirection = 0;
+    } else if(e.key == "ArrowRight") {
+        player.turnDirection = 0;
+    } else if(e.key == "ArrowLeft") {
+        player.turnDirection = 0;
+    }
+}
+function keyDownEvent(e)
+{
+    if(e.key == "ArrowUp"){
+        player.walkDirection = 1;
+    } else if(e.key == "ArrowDown") {
+        player.walkDirection = -1;
+    } else if(e.key == "ArrowRight") {
+        player.turnDirection = 1;
+    } else if(e.key == "ArrowLeft") {
+        player.turnDirection = -1;
+    }
+}
 function setup() {
     createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
-
+    document.addEventListener('keyup', keyUpEvent);
+    document.addEventListener('keydown', keyDownEvent);
 }
 
 function update() {
-    // TODO: update all game objects before we render the next frame
+    player.update();
 }
 
 function draw() {
     update();
 
     grid.render();
+    player.render();
 }
